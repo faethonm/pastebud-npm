d38c0680c5b9681efd88385a7d9a1ad5d51d722c
Faethon Milikouris <faethon.milikouris@gmail.com>
2018-05-13 21:41:18 -0700

    add comand line arguments

--- pastebud.js
+++ pastebud.js
@@ -1,6 +1,9 @@
 const pastebud = require('./'); // require the `index.js` file from the same directory.
-
-pastebud('send','sAAAAteady367', (err, resp) => {
+var args = process.argv.slice(2);
+console.log('args',args)
+const command = args[0];
+const content = args[1];
+pastebud(command, content, (err, resp) => {
     if (err) {
         console.log(err);
     }
